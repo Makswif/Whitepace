9397b2bcc979d72f64dcc43045d37ce8ba61c75f
Makswif <maksimdlaigr@gmail.com>
2024-11-25 18:12:48 +0200

    middle

--- javascript.js
+++ javascript.js
@@ -1,2 +1,6 @@
-var a = 36
-var b = 60
+var name = 'Ania';
+var  dodawanie = function(a,b,c) {return(a * b / c)};
+var  winik = dodawanie(2, 5, 7);
+console.log(winik)
+var x = 'ann\'a'
+console.log(x)
